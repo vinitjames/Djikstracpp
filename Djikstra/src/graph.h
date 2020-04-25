ad7dcd2b8d23920820c4d2e2e08255d9c31bd2b5
Vinit <vinit.james24@gmail.com>
2020-04-25 18:49:15 +0200

    add incomplete Graph

--- Djikstra/src/graph.h
+++ Djikstra/src/graph.h
@@ -4,6 +4,13 @@
 
 namespace Djikstra {
 	class Graph {
-		
+	public:
+		Graph();
+		~Graph();
+		inline const std::string GetName() const { return m_Name;}
+	private:
+		std::string m_Name;
+		std::vector<int> nodes;
+
 	};
 }
